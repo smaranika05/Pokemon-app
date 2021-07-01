e6269ab27989993db3225c4449014628214db525
smaranika05 <smaranikadatta05@gmail.com>
2021-07-02 01:04:06 +0530

    Updates

---
+++ precache-manifest.85141cc9c0e2f982b375ffda942e3195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80587d91ecdc8998c54840ea741e4a6",
+    "url": "/Pokemon-app/index.html"
+  },
+  {
+    "revision": "998b1a494f5af5677430",
+    "url": "/Pokemon-app/static/css/main.8f3f793e.chunk.css"
+  },
+  {
+    "revision": "bad8500d23c5a80a9a3d",
+    "url": "/Pokemon-app/static/js/2.6ceaa581.chunk.js"
+  },
+  {
+    "revision": "998b1a494f5af5677430",
+    "url": "/Pokemon-app/static/js/main.9ec207b6.chunk.js"
+  },
+  {
+    "revision": "06a983a52fcf43c638c9",
+    "url": "/Pokemon-app/static/js/runtime~main.7ba65cea.js"
+  },
+  {
+    "revision": "4602fe00c9d5caa9e5ae3a4e109e6535",
+    "url": "/Pokemon-app/static/media/pokeball.4602fe00.jpg"
+  }
+]);
